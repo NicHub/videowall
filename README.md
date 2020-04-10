dca9eceeb99ca2ccf11c550d106cf2c0c4fc4617
Utilisateur invité <Guest@nicobook-3.local>
2020-04-10 20:58:55 +0200

    New features

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## Prerequisites
 
-- A web browser (tested on Safari and Chrome on macOS).
+- A web browser
 - NodeJS (optional)
 
 ## Description
 
-It is a simple video wall that runs in a browser without the need for a web server. It has a fixed layout of 3 ✕ 3 videos. As browsers do not have access to the file system, the list of video files must be generated in a Javascript file called `playlist.js`.
+It is a simple video wall that runs in a browser without the need for a web server. The layout can be changed. As browsers do not have access to the file system, the list of video files must be generated in a Javascript file called `playlist.js`.
+
+It is especially useful to watch several videos one after the other with the 1×1 layout.
 
 ## playlist.js format
 
@@ -20,7 +22,7 @@ playlist = [
 
 ## Usage
 
-- Generate the Javascript file that will contain the path to the videos. This can be done by hand or with the following NodeJS script:
+- Generate the playlist.js file that will contain the path to the videos. This can be done by hand or with the following NodeJS script:
 
 ```bash
 node make_playlist.js
@@ -28,7 +30,26 @@ node make_playlist.js
 
 - Double click `videowall.html` to open it in your browser.
 
-- Use the mouse wheel to switch between videos.
+## Shortcuts
+
+- Press k to toggle play and pause in all players.
+- Use the mouse wheel to play next or previous videos in the player currently under the mouse cursor.
+- Press ← or → to play next or previous videos in all players.
+- Press ↓ or ↑ to change the layout.
+- Press a number from 0 to 4 to select a given layout.
+    - Press 0 for 1×1 layout.
+    - Press 1 for 1×2 layout.
+    - Press 2 for 2×2 layout.
+    - Press 3 for 3×3 layout.
+    - ...
+- Press s to shuffle the playlist order.
+
+## Web browser compatibility on macOS
+
+- Chrome, works best.
+- Safari, but the Developer tools need to be activated and the browser relaunched.
+- Firefox, works but the videos may not play at start, you need to press k, ← or → .
+
 
 <p align="center">
 <img height=400px src="https://github.com/NicHub/videowall/raw/master/videowall.jpg" />
--- videowall.js
+++ videowall.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const begin_at = 1 / 4;
-const allowed_nb_videos = [1, 2, 4, 9, 16];
 var nb_videos = 9;
+const allowed_nb_videos = [1, 2, 4, 9, 16, 25, 36, 49, 64, 81];
+var all_videos;
 
 main(nb_videos);
 
@@ -36,15 +37,17 @@ function main(nb_videos) {
     container.addEventListener("keyup", keyboard_shortcuts_management);
 
     // Duplicate video element.
+    all_videos = [video_elem];
     for (let index = 1; index < nb_videos; index++) {
-        let new_video = video_elem.cloneNode(true);
-        new_video.id = "v" + index;
-        video_elem.parentNode.insertBefore(new_video, last_video.nextSibling);
-        last_video = new_video;
+        let _video = video_elem.cloneNode(true);
+        _video.id = "v" + index;
+        if (video_elem.hasAttribute("muted")) _video.muted = true; // For FF.
+        video_elem.parentNode.insertBefore(_video, last_video.nextSibling);
+        last_video = _video;
+        all_videos.push(_video);
     }
 
     // Prepare each video.
-    var all_videos = container.getElementsByTagName("video");
     for (let index = 0; index < nb_videos; index++) {
 
         let _video = all_videos[index];
@@ -54,7 +57,7 @@ function main(nb_videos) {
         _video.setAttribute("data-moviepathidskey", 0);
 
         // `moviepathids` is an array that contains the indexes of
-        // the videos that the current video can play.
+        // the videos that the current player can play.
         let moviepathids = [];
         for (let key = index; key < playlist.length; key += nb_videos) {
             moviepathids.push(key);
@@ -65,28 +68,61 @@ function main(nb_videos) {
         _video.setAttribute("data-moviepathid", index);
 
         // Add wheel event.
-        _video.addEventListener("wheel", nextVideo, false);
+        _video.addEventListener("wheel", function () { nextVideo(this, event.deltaY < 0 ? -1 : +1) }, false);
 
         // Add ended event.
-        _video.addEventListener("ended", nextVideo, false);
+        _video.addEventListener("ended", function () { nextVideo(this, 1) }, false);
 
         // Set video source and play.
         setVideoSrcAndPlay(_video, playlist[index]);
     }
 }
 
-/***
- *
- **/
-function nextVideo() {
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(array) {
 
-    // Play next or previous video with mouse wheel event.
-    let _video = this;
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+/***
+ * Play next or previous video with mouse wheel event.
+ **/
+function nextVideo_all(increment) {
+
+    for (let index = 0; index < nb_videos; index++) {
+        let _video = all_videos[index];
+        nextVideo(_video, increment)
+    }
+}
+
+/***
+ * Play next or previous video with mouse wheel event.
+ **/
+function nextVideo(_video, increment) {
 
     // Get and Set movie metadata.
     let moviepathidskey = parseInt(_video.getAttribute("data-moviepathidskey"));
     let moviepathids = _video.getAttribute("data-moviepathids").split(",");
-    let increment = event.deltaY < 0 ? -1 : +1; // If event.deltaY is undefined, then increment will be 1;
+    // let increment = event.deltaY < 0 ? -1 : +1; // If event.deltaY is undefined, then increment will be 1;
     moviepathidskey = (moviepathidskey + increment) % (moviepathids.length);
     moviepathidskey = moviepathidskey < 0 ? (moviepathids.length + moviepathidskey) : moviepathidskey;
     _video.setAttribute("data-moviepathidskey", moviepathidskey);
@@ -120,24 +156,80 @@ function setVideoSrcAndPlay(_video, _src) {
 /***
  *
  **/
-function keyboard_shortcuts_management(evt) {
+function change_layout(layout_id) {
 
     const cur_id = allowed_nb_videos.indexOf(nb_videos);
     const nb_id = allowed_nb_videos.length;
     var next_id;
-    if (["+", "ArrowUp"].includes(event.key)) {
+    if (layout_id == "increase") {
         next_id = (cur_id + 1) % nb_id;
     }
-    else if (["-", "ArrowDown"].includes(event.key)) {
+    else if (layout_id == "decrease") {
         next_id = (cur_id - 1);
         next_id = (next_id >= 0) ? next_id : nb_id - 1;
     }
-    else if ([" "].includes(event.key)) {
-        console.log("space");
+    else if (layout_id >= 0 && layout_id < allowed_nb_videos.length) {
+        next_id = layout_id;
     }
     else {
         return;
     }
+
     nb_videos = allowed_nb_videos[next_id];
     main(nb_videos);
+}
+
+/***
+ *
+ **/
+function togglePlayPauseAll() {
+
+    for (let index = 0; index < nb_videos; index++) {
+        let _video = all_videos[index];
+        const isVideoPlaying = !!(_video.currentTime > 0 && !_video.paused && !_video.ended && _video.readyState > 2);
+        if (isVideoPlaying) {
+            var playPromise = _video.pause();
+            if (playPromise !== undefined) {
+                playPromise.catch(_ => { });
+            }
+        }
+        else {
+            var playPromise = _video.play();
+            if (playPromise !== undefined) {
+                playPromise.catch(_ => { });
+            }
+        }
+    }
+}
+
+/***
+ *
+ **/
+function keyboard_shortcuts_management(event) {
+
+    if (["ArrowUp"].includes(event.key)) {
+        change_layout("increase");
+    }
+    else if (["ArrowDown"].includes(event.key)) {
+        change_layout("decrease");
+    }
+    else if (!isNaN(event.key)) {
+        change_layout(parseInt(event.key));
+    }
+    else if (["ArrowRight"].includes(event.key)) {
+        nextVideo_all(1);
+    }
+    else if (["ArrowLeft"].includes(event.key)) {
+        nextVideo_all(-1);
+    }
+    else if (["k"].includes(event.key)) {
+        togglePlayPauseAll();
+    }
+    else if (["s"].includes(event.key)) {
+        playlist = shuffle(playlist);
+        main(nb_videos);
+    }
+    else {
+        return;
+    }
 }
