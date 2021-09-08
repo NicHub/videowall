407b7c3a59c8d79a3d4452f4fb031b5cacbb0b63
Nicolas Jeanmonod <nicolas.jeanmonod@gmail.com>
2021-09-08 12:40:43 +0200

    refactoring + url encode

--- assets/make_playlist.py
+++ assets/make_playlist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-
+import urllib.parse
 
 # The file name must end with one of EXTENSIONS.
 # If EXTENSIONS is empty, this test is skipped.
@@ -12,14 +12,19 @@ EXTENSIONS = ("mp4", "webm", "m4v")
 EXCLUDES = ("novideowall", "404.mp4", "empty.mp4")
 
 PATH_TO_WALK = "../../videos/"
-FILE_NAME = "playlist.js"
+PLAYLIST_FILE_NAME = "playlist.js"
 
 
 def list_files(path_to_walk):
     """ ___ """
 
     filtered_files = []
+    hidden_files = []
+    non_supported_extension_files = []
+    files_with_excludes = []
+
     for root, _, files in os.walk(path_to_walk):
+
         for file in files:
 
             # Make path name relative to this script.
@@ -27,12 +32,13 @@ def list_files(path_to_walk):
 
             # Test if it is a hidden file.
             if file.startswith("."):
-                print(f'Hidden file found:\n"{file_name}"')
+                hidden_files.append(os.path.abspath(file_name))
                 continue
 
             # Test if file extension is OK.
             if len(EXTENSIONS) > 0 and not file_name.endswith(EXTENSIONS):
-                print(f'File with non supported extension found:\n"{file_name}"')
+                non_supported_extension_files.append(
+                    os.path.abspath(file_name))
                 continue
 
             # Test if exclude string found.
@@ -40,19 +46,27 @@ def list_files(path_to_walk):
             for exclude in EXCLUDES:
                 if file_name.find(exclude) > -1:
                     exclude_found = True
-                    print(f'File with exclude string found:\n"{file_name}"')
                     break
             if exclude_found:
+                files_with_excludes.append(os.path.abspath(file_name))
                 continue
 
             # If we arrived here, the file is OK.
-            # Make its path name relative to videowall.html
-            # and add it to the list.
+            # Make its path name relative to videowall.html,
+            # URL encode it and add it to the list.
             file_name = file_name[3:]
+            file_name = urllib.parse.quote(file_name)
             filtered_files.append(file_name)
 
+    # Sort the list of filtered files and return it.
     filtered_files.sort()
-    return filtered_files
+    files = {
+        "filtered_files": filtered_files,
+        "hidden_files": hidden_files,
+        "non_supported_extension_files": non_supported_extension_files,
+        "files_with_excludes": files_with_excludes
+    }
+    return files
 
 
 def make_playlist(filtered_files):
@@ -61,6 +75,7 @@ def make_playlist(filtered_files):
     # If the playlist is empty.
     if len(filtered_files) == 0:
         playlist = "playlist = []"
+        print("CREATING EMPTY PLAYLIST")
         return playlist
 
     # If the playlist is not empty.
@@ -70,6 +85,7 @@ def make_playlist(filtered_files):
         f"{playlist}"
         f'"\n]\n'
     )
+    print(f"CREATING PLAYLIST WITH {len(filtered_files)} MOVIES.")
     return playlist
 
 
@@ -79,10 +95,33 @@ def save_playlist(playlist, file_name):
     f = open(file_name, "w")
     f.write(playlist)
     f.close()
+    print(f'PLAYLIST SAVED IN "{os.path.abspath(file_name)}"')
+
+
+def report_alien_files(files):
+    """ ___ """
+
+    # Print the list of hidden files.
+    if len(files["hidden_files"]) > 0:
+        msg = '" \\\n   "'.join(files["hidden_files"])
+        msg = f'rm \\\n   "{msg}"'
+        print(f'\n{len(files["hidden_files"])} HIDDEN FILE(S) FOUND:\n{msg}')
+
+    # Print the list of files with non supported extentions.
+    if len(files["non_supported_extension_files"]) > 0:
+        print(
+            f'\n{len(files["non_supported_extension_files"])} FILE(S) WITH NON SUPPORTED EXTENSION FOUND:')
+        print('"' + '"\n"'.join(files["non_supported_extension_files"]) + '"')
+
+    # Print the list of files with excludes.
+    if len(files["files_with_excludes"]) > 0:
+        print(f'\n{len(files["files_with_excludes"])} FILE(S) WITH EXCLUDE STRING FOUND:')
+        print('"' + '"\n"'.join(files["files_with_excludes"]) + '"')
 
 
 if __name__ == "__main__":
 
-    filtered_files = list_files(PATH_TO_WALK)
-    playlist = make_playlist(filtered_files)
-    save_playlist(playlist, FILE_NAME)
+    files = list_files(PATH_TO_WALK)
+    playlist = make_playlist(files["filtered_files"])
+    save_playlist(playlist, PLAYLIST_FILE_NAME)
+    report_alien_files(files)
